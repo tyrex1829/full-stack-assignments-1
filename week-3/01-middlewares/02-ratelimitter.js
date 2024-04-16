c52bd473fe6c0446489bf9eb6e04e30bc088a5c4
Saksham Khare <sakshamkhare19@gmail.com>
2024-04-16 16:09:58 +0530

    complete 01 of week 3

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,22 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req, res, next){
+  const userId = req.headers["user-id"];
+
+  if(numberOfRequestsForUser[userId]){
+    numberOfRequestsForUser[userId] = numberOfRequestsForUser[userId] + 1;
+    if(numberOfRequestsForUser[userId] > 5){
+      res.status(404).send("not allowed reload after a second");
+    } else{
+      next();
+    }
+  } else{
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
